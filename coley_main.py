e190345a4a52f2ce163beffcd5b86e276580f8b2
Joearrowsmith <joearrowsmith98@gmail.com>
2023-02-08 00:31:54 +0000

    adding the prediction network for the coley model

--- coley_main.py
+++ coley_main.py
@@ -43,8 +43,8 @@ rng = np.random.default_rng(12345)
 indexes = np.arange(df.shape[0])
 rng.shuffle(indexes)
 
-train_test_split = 0.1
-train_val_split = 0.1
+train_test_split = 0.9
+train_val_split = 0.9
 
 test_idx = indexes[int(df.shape[0] * train_test_split):]
 train_val_idx = indexes[:int(df.shape[0] * train_test_split)]
@@ -66,7 +66,7 @@ train_reagents_1, val_reagents_1, reag1_enc = src.learn.ohe.apply_train_ohe_fit(
 train_temperature = tf.convert_to_tensor(df['temperature_0'].iloc[train_idx].fillna(0.))
 val_temperature = tf.convert_to_tensor(df['temperature_0'].iloc[val_idx].fillna(0.))
 
-# del df
+del df
 
 print("Loaded data")
 
@@ -76,19 +76,18 @@ x_train_data = (
     train_product_fp, 
     train_rxn_diff_fp, 
     train_catalyst, 
-    train_reagents_0, 
-    train_reagents_1, 
     train_solvent_0, 
     train_solvent_1, 
-    # train_temperature, 
+    train_reagents_0, 
+    train_reagents_1, 
 )
 
 y_train_data = (
     train_catalyst, 
-    train_reagents_0, 
-    train_reagents_1, 
     train_solvent_0, 
     train_solvent_1, 
+    train_reagents_0, 
+    train_reagents_1, 
     train_temperature, 
 )
 
@@ -96,24 +95,24 @@ x_val_data = (
     val_product_fp, 
     val_rxn_diff_fp, 
     val_catalyst, 
-    val_reagents_0, 
-    val_reagents_1, 
     val_solvent_0, 
     val_solvent_1, 
+    val_reagents_0, 
+    val_reagents_1, 
 )
 
 y_val_data = (
     val_catalyst, 
-    val_reagents_0, 
-    val_reagents_1, 
     val_solvent_0, 
     val_solvent_1, 
+    val_reagents_0, 
+    val_reagents_1, 
     val_temperature, 
 )
 
 # %%
 
-model = src.coley_code.model.build(
+model = src.coley_code.model.build_teacher_forcing_model(
     pfp_len=train_product_fp.shape[-1], 
     rxnfp_len=train_rxn_diff_fp.shape[-1], 
     c1_dim=train_catalyst.shape[-1], # TODO check not top 100 
@@ -124,11 +123,26 @@ model = src.coley_code.model.build(
     N_h1=1024, 
     N_h2=100,
     l2v=0, # TODO check what coef they used
-    lr=1e-4,
+    use_hard_selection=False,
+)
+
+# we use a separate model for prediction because we use a recurrent setup for prediction
+pred_model = src.coley_code.model.build_teacher_forcing_model(
+    pfp_len=train_product_fp.shape[-1], 
+    rxnfp_len=train_rxn_diff_fp.shape[-1], 
+    c1_dim=train_catalyst.shape[-1],
+    s1_dim=train_solvent_0.shape[-1], 
+    s2_dim=train_solvent_1.shape[-1], 
+    r1_dim=train_reagents_0.shape[-1], 
+    r2_dim=train_reagents_1.shape[-1], 
+    N_h1=1024, 
+    N_h2=100,
+    l2v=0,
+    use_hard_selection=True,
 )
 
 def mse_ignore_na(y_true, y_pred):
-    raise ValueError("This function messes up with autograd")
+    raise ValueError("This function messes up with autograd, currently we are setting the values to zero, in the future probably need to separate this data out")
     return tf.reduce_mean(tf.keras.backend.switch(tf.math.is_nan(y_true), tf.zeros_like(y_true),(y_pred - y_true)**2))
 
 
@@ -143,12 +157,46 @@ model.compile(
     ], 
     loss_weights=[1, 1, 1, 1, 1, 1e-4], 
     optimizer='adam',
+    metrics = {
+        'c1':['acc', tf.keras.metrics.TopKCategoricalAccuracy(k=3, name="top3"), tf.keras.metrics.TopKCategoricalAccuracy(k=5, name="top5")],
+        's1':['acc', tf.keras.metrics.TopKCategoricalAccuracy(k=3, name="top3"), tf.keras.metrics.TopKCategoricalAccuracy(k=5, name="top5")],
+        's2':['acc', tf.keras.metrics.TopKCategoricalAccuracy(k=3, name="top3"), tf.keras.metrics.TopKCategoricalAccuracy(k=5, name="top5")],
+        'r1':['acc', tf.keras.metrics.TopKCategoricalAccuracy(k=3, name="top3"), tf.keras.metrics.TopKCategoricalAccuracy(k=5, name="top5")],
+        'r2':['acc', tf.keras.metrics.TopKCategoricalAccuracy(k=3, name="top3"), tf.keras.metrics.TopKCategoricalAccuracy(k=5, name="top5")],
+    },
 )
+
+# %%
+
+src.coley_code.model.update_teacher_forcing_model_weights(update_model=pred_model, to_copy_model=model)
+
+cat_pred = model(x_train_data)[0]
+print("true", (pd.Series(train_catalyst.numpy().argmax(axis=1)).value_counts() / train_catalyst.shape[0]).iloc[:5], sep="\n")
+print("pred", (pd.Series(cat_pred.numpy().argmax(axis=1)).value_counts() / cat_pred.shape[0]).iloc[:5], sep="\n")
+
+cat_pred = pred_model(x_train_data)[0]
+print("[pred] true", (pd.Series(train_catalyst.numpy().argmax(axis=1)).value_counts() / train_catalyst.shape[0]).iloc[:5], sep="\n")
+print("[pred] pred", (pd.Series(cat_pred.numpy().argmax(axis=1)).value_counts() / cat_pred.shape[0]).iloc[:5], sep="\n")
+
+# %%
 
 h = model.fit(
-    x=x_train_data, y=y_train_data, epochs=20, verbose=1, validation_data=(x_val_data, y_val_data),
+    x=x_train_data, y=y_train_data, 
+    epochs=1, verbose=1, batch_size=5096,
+    validation_data=(x_val_data, y_val_data),
 )
 
+# %%
+
+src.coley_code.model.update_teacher_forcing_model_weights(update_model=pred_model, to_copy_model=model)
+
+cat_pred = model(x_train_data)[0]
+print("true", (pd.Series(train_catalyst.numpy().argmax(axis=1)).value_counts() / train_catalyst.shape[0]).iloc[:5], sep="\n")
+print("pred", (pd.Series(cat_pred.numpy().argmax(axis=1)).value_counts() / cat_pred.shape[0]).iloc[:5], sep="\n")
+
+cat_pred = pred_model(x_train_data)[0]
+print("[pred] true", (pd.Series(train_catalyst.numpy().argmax(axis=1)).value_counts() / train_catalyst.shape[0]).iloc[:5], sep="\n")
+print("[pred] pred", (pd.Series(cat_pred.numpy().argmax(axis=1)).value_counts() / cat_pred.shape[0]).iloc[:5], sep="\n")
 
 # %%
 
@@ -156,6 +204,16 @@ import matplotlib.pyplot as plt
 
 plt.plot(h.history['loss'], label="loss")
 plt.plot(h.history['val_loss'], label="val_loss")
+plt.legend()
+
+# %%
+
+plt.plot(h.history['c1_acc'], label="c1_acc")
+plt.plot(h.history['c1_top3'], label="c1_top3")
+plt.plot(h.history['c1_top5'], label="c1_top5")
+plt.plot(h.history['val_c1_acc'], label="val_c1_acc")
+plt.plot(h.history['val_c1_top3'], label="val_c1_top3")
+plt.plot(h.history['val_c1_top5'], label="val_c1_top5")
 plt.legend()
 
 # %%
--- src/coley_code/model.py
+++ src/coley_code/model.py
@@ -1,83 +1,124 @@
 import tensorflow as tf
 
 
-def build(pfp_len = 2048, rxnfp_len = 2048, c1_dim = 100, s1_dim = 100, s2_dim =100, r1_dim = 100, r2_dim =100, N_h1 = 1024, N_h2 = 100,l2v = 0, lr = 0.01):
-	input_pfp = tf.keras.layers.Input(shape = (pfp_len,), name = 'input_pfp')
-	input_rxnfp = tf.keras.layers.Input(shape = (rxnfp_len,), name = 'input_rxnfp')
+def hard_selection(pred):
+    return tf.stop_gradient(
+        tf.one_hot(
+            tf.math.argmax(pred, axis=1), pred.shape[1]
+        )
+    )
 
-	input_r1 = tf.keras.layers.Input(shape = (r1_dim,), name = 'input_r1')
-	input_r2 = tf.keras.layers.Input(shape = (r2_dim,), name = 'input_r2')
-	input_s1 = tf.keras.layers.Input(shape = (s1_dim,), name = 'input_s1')
-	input_s2 = tf.keras.layers.Input(shape = (s2_dim,), name = 'input_s2')
-	input_c1 = tf.keras.layers.Input(shape = (c1_dim,), name = 'input_c1')
-	
 
-	concat_fp = tf.keras.layers.Concatenate(axis = 1)([input_pfp,input_rxnfp])
+def build_teacher_forcing_model(
+	pfp_len=2048, rxnfp_len=2048, c1_dim=100, s1_dim=100, s2_dim=100, r1_dim=100, r2_dim=100, N_h1=1024, N_h2=100, l2v=0, use_hard_selection=False
+) -> tf.keras.models.Model:
+    input_pfp = tf.keras.layers.Input(shape = (pfp_len,), name = 'input_pfp')
+    input_rxnfp = tf.keras.layers.Input(shape = (rxnfp_len,), name = 'input_rxnfp')
 
-	h1 = tf.keras.layers.Dense(1000, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v),name = 'fp_transform1')(concat_fp)
-	# h1_dropout = Dropout(0.3)(h1)
-	h2 = tf.keras.layers.Dense(1000, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v),name = 'fp_transform2')(h1)
-	h2_dropout = tf.keras.layers.Dropout(0.5)(h2,training=False)
-	# h1 = concat_fp
+    if not use_hard_selection:
+        input_c1 = tf.keras.layers.Input(shape = (c1_dim,), name = 'input_c1')
+        input_s1 = tf.keras.layers.Input(shape = (s1_dim,), name = 'input_s1')
+        input_s2 = tf.keras.layers.Input(shape = (s2_dim,), name = 'input_s2')
+        input_r1 = tf.keras.layers.Input(shape = (r1_dim,), name = 'input_r1')
+        input_r2 = tf.keras.layers.Input(shape = (r2_dim,), name = 'input_r2')
 
-	c1_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = 'c1_h1')(h2_dropout)
-	c1_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = 'c1_h2')(c1_h1)
-	# c1_h2_dropout = Dropout(0.3)(c1_h2)
-	c1_output = tf.keras.layers.Dense(c1_dim, activation = "softmax",name = "c1")(c1_h2)
-	c1_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 'c1_dense')(input_c1)
+    concat_fp = tf.keras.layers.Concatenate(axis = 1)([input_pfp,input_rxnfp])
 
-	concat_fp_c1 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1")([h2_dropout,c1_dense])
+    h1 = tf.keras.layers.Dense(1000, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v),name = 'fp_transform1')(concat_fp)
+    # h1_dropout = Dropout(0.3)(h1)
+    h2 = tf.keras.layers.Dense(1000, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v),name = 'fp_transform2')(h1)
+    h2_dropout = tf.keras.layers.Dropout(0.5)(h2,training=False)
+    # h1 = concat_fp
 
-	s1_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s1_h1")(concat_fp_c1)
-	s1_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s1_h2")(s1_h1)
-	# s1_h2_dropout = Dropout(0.3)(s1_h2)
-	s1_output = tf.keras.layers.Dense(s1_dim, activation = "softmax", name = "s1")(s1_h2)
-	s1_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 's1_dense')(input_s1)
-	# rgt_output = Lambda(lambda x: x / K.sum(x, axis=-1),output_shape = (rgt_dim,))(rgt_unscaled)
+    c1_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = 'c1_h1')(h2_dropout)
+    c1_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = 'c1_h2')(c1_h1)
+    # c1_h2_dropout = Dropout(0.3)(c1_h2)
+    c1_output = tf.keras.layers.Dense(c1_dim, activation = "softmax",name = "c1")(c1_h2)
+    if use_hard_selection:
+        input_c1 = hard_selection(c1_output)
+    c1_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 'c1_dense')(input_c1)
 
-	concat_fp_c1_s1 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1")([h2_dropout,c1_dense,s1_dense])
-	
-	s2_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s2_h1")(concat_fp_c1_s1)
-	s2_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s2_h2")(s2_h1)
-	# s2_h2_dropout = Dropout(0.3)(s2_h2)
-	s2_output = tf.keras.layers.Dense(s2_dim, activation = "softmax", name = "s2")(s2_h2)	
-	s2_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 's2_dense')(input_s2)
+    concat_fp_c1 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1")([h2_dropout,c1_dense])
 
-	concat_fp_c1_s1_s2 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1_s2")([h2_dropout,c1_dense, s1_dense, s2_dense])
+    s1_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s1_h1")(concat_fp_c1)
+    s1_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s1_h2")(s1_h1)
+    # s1_h2_dropout = Dropout(0.3)(s1_h2)
+    s1_output = tf.keras.layers.Dense(s1_dim, activation = "softmax", name = "s1")(s1_h2)
+    if use_hard_selection:
+        input_s1 = hard_selection(s1_output)
+    s1_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 's1_dense')(input_s1)
+    # rgt_output = Lambda(lambda x: x / K.sum(x, axis=-1),output_shape = (rgt_dim,))(rgt_unscaled)
 
-	r1_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r1_h1")(concat_fp_c1_s1_s2)
-	r1_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r1_h2")(r1_h1)
-	# r1_h2_dropout = Dropout(0.3)(r1_h2)
-	r1_output = tf.keras.layers.Dense(r1_dim, activation = "softmax", name = "r1")(r1_h2)
-	r1_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 'r1_dense')(input_r1)
-	# rgt_output = Lambda(lambda x: x / K.sum(x, axis=-1),output_shape = (rgt_dim,))(rgt_unscaled)
+    concat_fp_c1_s1 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1")([h2_dropout,c1_dense,s1_dense])
 
-	concat_fp_c1_s1_s2_r1 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1_s2_r1")([h2_dropout,c1_dense,s1_dense,s2_dense,r1_dense])
-	
-	r2_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r2_h1")(concat_fp_c1_s1_s2_r1)
-	r2_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r2_h2")(r2_h1)
-	# r2_h2_dropout = Dropout(0.3)(r2_h2)
-	r2_output = tf.keras.layers.Dense(r2_dim, activation = "softmax", name = "r2")(r2_h2)
-	r2_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 'r2_dense')(input_r2)	
+    s2_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s2_h1")(concat_fp_c1_s1)
+    s2_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "s2_h2")(s2_h1)
+    # s2_h2_dropout = Dropout(0.3)(s2_h2)
+    s2_output = tf.keras.layers.Dense(s2_dim, activation = "softmax", name = "s2")(s2_h2)    
+    if use_hard_selection:
+        input_s2 = hard_selection(s2_output)
+    s2_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 's2_dense')(input_s2)
 
-	concat_fp_c1_s1_s2_r1_r2 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1_s2_r1_r2")([h2_dropout,c1_dense,s1_dense,s2_dense,r1_dense,r2_dense])
-	
-	T_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', name = "T_h1")(concat_fp_c1_s1_s2_r1_r2)
-	# T_h1_dropout = Dropout(0.3)(T_h1)
-	# T_h2 = keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = l2(l2v), name = "T_h2")(T_h1)
-	T_output = tf.keras.layers.Dense(1, activation = "linear", name = "T")(T_h1)	
-	#just for the purpose of shorter print message
-	c1 = c1_output
-	r1 = r1_output
-	r2 = r2_output
-	s1 = s1_output
-	s2 = s2_output
-	Temp = T_output
-	output = [c1,r1,r2,s1,s2,Temp]
-	model = tf.keras.models.Model([input_pfp,input_rxnfp,input_c1,input_r1,input_r2,input_s1,input_s2],output)
-	# print(model.output_shape)
+    concat_fp_c1_s1_s2 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1_s2")([h2_dropout,c1_dense, s1_dense, s2_dense])
 
-	model.count_params()
-	model.summary()
+    r1_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r1_h1")(concat_fp_c1_s1_s2)
+    r1_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r1_h2")(r1_h1)
+    # r1_h2_dropout = Dropout(0.3)(r1_h2)
+    r1_output = tf.keras.layers.Dense(r1_dim, activation = "softmax", name = "r1")(r1_h2)
+    if use_hard_selection:
+        input_r1 = hard_selection(r1_output)
+    r1_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 'r1_dense')(input_r1)
+    # rgt_output = Lambda(lambda x: x / K.sum(x, axis=-1),output_shape = (rgt_dim,))(rgt_unscaled)
 
-	return model
+    concat_fp_c1_s1_s2_r1 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1_s2_r1")([h2_dropout,c1_dense,s1_dense,s2_dense,r1_dense])
+
+    r2_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r2_h1")(concat_fp_c1_s1_s2_r1)
+    r2_h2 = tf.keras.layers.Dense(N_h1, activation = 'tanh', kernel_regularizer = tf.keras.regularizers.l2(l2v), name = "r2_h2")(r2_h1)
+    # r2_h2_dropout = Dropout(0.3)(r2_h2)
+    r2_output = tf.keras.layers.Dense(r2_dim, activation = "softmax", name = "r2")(r2_h2)
+    if use_hard_selection:
+        input_r2 = hard_selection(r2_output)
+    r2_dense = tf.keras.layers.Dense(N_h2, activation = 'relu',name = 'r2_dense')(input_r2)    
+
+    concat_fp_c1_s1_s2_r1_r2 = tf.keras.layers.Concatenate(axis = -1,name = "concat_fp_c1_s1_s2_r1_r2")([h2_dropout,c1_dense,s1_dense,s2_dense,r1_dense,r2_dense])
+
+    T_h1 = tf.keras.layers.Dense(N_h1, activation = 'relu', name = "T_h1")(concat_fp_c1_s1_s2_r1_r2)
+    # T_h1_dropout = Dropout(0.3)(T_h1)
+    # T_h2 = keras.layers.Dense(N_h1, activation = 'relu', kernel_regularizer = l2(l2v), name = "T_h2")(T_h1)
+    T_output = tf.keras.layers.Dense(1, activation = "linear", name = "T")(T_h1)    
+    #just for the purpose of shorter print message
+    c1 = c1_output
+    s1 = s1_output
+    s2 = s2_output
+    r1 = r1_output
+    r2 = r2_output
+    Temp = T_output
+    output = [c1,s1,s2,r1,r2,Temp]
+    if use_hard_selection:
+        model = tf.keras.models.Model(
+            [input_pfp, input_rxnfp],
+            output
+        )
+    else:
+        model = tf.keras.models.Model(
+            [input_pfp, input_rxnfp, input_c1, input_s1, input_s2, input_r1, input_r2],
+            output
+        )
+
+    model.count_params()
+    model.summary()
+    return model
+
+
+def update_teacher_forcing_model_weights(update_model, to_copy_model):
+    layers = [
+        'fp_transform1', 'fp_transform2', 'c1_dense', 's1_dense', 
+        's2_dense', 'r1_dense', 'r2_dense', 'c1_h1', 's1_h1', 's2_h1', 
+        'r1_h1', 'r2_h1', 'c1_h2', 's1_h2', 's2_h2', 'r1_h2', 'r2_h2', 
+        'T_h1', 'c1', 's1', 's2', 'r1', 'r2', 'T'
+    ]
+
+    for l in layers:
+        update_model.get_layer(l).set_weights(
+            to_copy_model.get_layer(l).get_weights()
+        )
